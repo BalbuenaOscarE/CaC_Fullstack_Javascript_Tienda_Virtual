e6a406b2c46f1a814a6f2c7098f56e7ff2d8c102
BalbuenaOscarE <balbuenaoscar5@gmail.com>
2023-09-16 21:37:10 -0300

    0.9

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,98 @@
-const menu = document.getElementById("menu")
+const menu_expand = document.getElementById("menu_expand");
 
-menu.addEventListener("click", () => {
+const menu_contract = document.getElementById("menu_contract");
 
-    item_1.style.display = "flex";
+const navbar_ul = document.getElementById("navbar_ul");
 
-    item_2.style.display = "flex";
+const item_1 = document.getElementById("item_1");
 
-    item_3.style.display = "flex";
+const item_2 = document.getElementById("item_2");
 
-    item_4.style.display = "flex";
+const item_3 = document.getElementById("item_3");
+
+const item_4 = document.getElementById("item_4");
+
+
+function disable_navbar(){
+
+    navbar_ul.style.flexDirection = "column";
+    
+    menu_expand.style.marginTop = "5vw"
+        
+    item_1.style.display = "initial";
+    
+    item_1.style.width = "100%";
+    
+    item_2.style.display = "initial";
+    
+    item_2.style.width = "100%";
+    
+    item_3.style.display = "initial";
+        
+    item_3.style.width = "100%";
+    
+    item_4.style.display = "initial";
+        
+    item_4.style.width = "100%";
+
+}
+
+function enable_navbar(){
+
+    navbar_ul.style.display = "flex";
+        
+    navbar_ul.style.flexDirection = "row";
+    
+    menu_expand.style.marginTop = "5vw"
+        
+    item_1.style.display = "none";
+    
+    item_1.style.width = "fit-content";
+    
+    item_2.style.display = "none";
+    
+    item_2.style.width = "fit-content";
+    
+    item_3.style.display = "none";
+        
+    item_3.style.width = "fit-content";
+    
+    item_4.style.display = "none";
+        
+    item_4.style.width = "fit-content";
+
+}
+
+function expand(){
+
+
+    menu_contract.style.display = "block";
+
+    menu_expand.style.display = "none";
+
+}
+
+function contract(){
+
+    menu_contract.style.display = "none";
+
+    menu_expand.style.display = "block";
+
+}
+
+
+menu_expand.addEventListener("click", ()=>{
+
+    disable_navbar();
+
+    expand();
+
+})
+
+menu_contract.addEventListener("click", ()=>{
+
+    enable_navbar();
+
+    contract();
 
 })
