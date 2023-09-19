0a5027eba41ec8cb94714e2bbae5da88b362f3ee
BalbuenaOscarE <balbuenaoscar5@gmail.com>
2023-09-19 19:19:18 -0300

    0.15

--- js/index_slider.js
+++ js/index_slider.js
@@ -7,9 +7,6 @@ let sliderSectionLast = sliderSection[sliderSection.length -1];
 
 // el largo de la lista, le restamos uno, y nos devuelve la última posición, ésto lo leemos cómo un index y ya ubicamos la última sección
 
-let btnLeft = document.querySelector("#btn-left");
-let btnRight = document.querySelector("#btn-right");
-
 slider.insertAdjacentElement("afterbegin",sliderSectionLast)
 
 //inserta el elemento sliderSectionLast, despues de empezar, es decir, en la posición [0]
@@ -33,48 +30,10 @@ slider.style.marginLeft = "-100%"
 
 }
 
-btnRight.addEventListener("click", function(){
-
-    Next();
-
-}
-
-)
-
-function Prev(){
-
-    let sliderSection = document.querySelectorAll(".slider_section");
-
-    let sliderSectionLast = sliderSection[sliderSection.length -1];
-
-    slider.style.marginLeft = "0%";
-
-    slider.style.transition = "all 0.5s";
-
-    setTimeout(function(){
-
-slider.style.transition = "none";
-
-slider.insertAdjacentElement("afterbegin",sliderSectionLast);
-
-slider.style.marginLeft = "-100%"
-
-    },500);
-
-}
-
-btnLeft.addEventListener("click", function(){
-
-    Prev();
-
-}
-
-)
-
-/* Para ponerlo automático(cada 5000 ms)
+/*Para ponerlo automático(cada 5000 ms)*/
 
 setInterval(function(){ 
 
     Next();
 
-},5000) */
+},5000)
